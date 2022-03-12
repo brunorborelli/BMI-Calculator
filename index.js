751e388542dacc92e9d64adb2fc896d72c60b93e
BrunoBorelli <rodriguesborellibruno@gmail.com>
2022-03-12 13:06:12 -0300

    first commit IMC(BMI) CALCULATOR

---
+++ index.js
@@ -0,0 +1,14 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura').value;
+const peso = document.getElementById('peso').value;
+const calcular = document.getElementById('calcular');
+const resultado = document.getElementById('resultado');
+
+
+function imc (){
+
+    nome.value = "chupa meu pau"
+     
+}
+
+calcular.addEventListener('click', imc );
